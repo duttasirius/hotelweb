49122559894a19cae592f8270f6d3e36979decaa
Sagnik Dutta <sagnikdutta1999@gmail.com>
2026-09-09 20:41:04 +0530

    refactor(auth): remove Clerk middleware and webhook

--- server/server.js
+++ server/server.js
@@ -2,29 +2,23 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import { clerkMiddleware } from "@clerk/express";
-import clerkWebhooks from "./controllers/clerWebhook.js";
+import authRouter from "./routers/authRoutes.js";
 import userRouter from "./routers/userRoutes.js";
 import hotelRouter from "./routers/hotelRoutes.js";
 import connectCloudinary from "./config/cloudinary.js";
 import roomRouter from "./routers/roomRoutes.js";
 import bookingRouter from "./routers/bookingRoutes.js";
-
-// ADD THIS
-import { v2 as cloudinary } from "cloudinary";
 import paymentRouter from "./routers/paymentRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(clerkMiddleware());
 
 await connectDB();
 await connectCloudinary();
 
-app.post("/api/clerk", clerkWebhooks);
-
+app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/room", roomRouter);
