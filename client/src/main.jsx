bbb1097d613becb535353dd605b688035de2411e
Sagnik Dutta <sagnikdutta1999@gmail.com>
2026-09-09 20:41:08 +0530

    refactor(auth): remove Clerk provider from client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/react";
 
 import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./context/AppContex.jsx";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </ClerkProvider>,
+  <BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </BrowserRouter>,
 );
