31024dbcc7cae9ef16baca5cf1be3d28fc70edad
Sagnik Dutta <sagnikdutta1999@gmail.com>
2026-09-09 20:41:19 +0530

    refactor(auth): mount JWT auth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./context/AppContex.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </AuthProvider>
   </BrowserRouter>,
 );
